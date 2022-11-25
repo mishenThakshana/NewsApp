c5622d65a018840e4dca8af39ea230b0e9e88996
mishenThakshana <mishen.git@gmail.com>
2022-11-26 03:12:24 +0530

    News Detail Layout

--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -1,8 +1,8 @@
 export default {
   LOGIN: 'Login',
   REGISTER: 'Register',
-  HOME_NAVIGATOR: 'HomeNavigator',
-  HOME: 'Home',
+  HOME_NAVIGATOR: 'Home',
+  HOME: 'HomeScreen',
   NEWS_DETAIL: 'NewsDetail',
   FAVOURITES: 'Favourites',
   PROFILE: 'Profile',
--- src/screens/main/subpages/NewsDetail.tsx
+++ src/screens/main/subpages/NewsDetail.tsx
@@ -1,7 +1,14 @@
 import {FC} from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Image,
+  useWindowDimensions,
+  TouchableOpacity,
+} from 'react-native';
 import {colors} from 'src/constants';
 import styles from 'src/styles/Common.styles';
+import MaterialIcon from 'react-native-vector-icons/Ionicons';
 
 interface NewsDetailInterface {
   navigation: any;
@@ -10,11 +17,62 @@ interface NewsDetailInterface {
 
 const NewsDetail: FC<NewsDetailInterface> = ({navigation, route}) => {
   const {article} = route.params;
-  console.log(article);
+  const {width, height} = useWindowDimensions();
 
   return (
     <SafeAreaView style={styles.rootContainer}>
-      <Text style={{color: colors.DARK}}>{article.content}</Text>
+      <View
+        style={{
+          zIndex: 10,
+          backgroundColor: 'rgba(245, 245, 245,0.5)',
+          alignSelf: 'flex-start',
+          margin: 15,
+          padding: 8,
+          alignItems: 'center',
+          borderRadius: 10,
+        }}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <MaterialIcon
+            onPress={() => navigation.goBack()}
+            name="arrow-back-outline"
+            color={colors.DARK}
+            size={22}
+          />
+        </TouchableOpacity>
+      </View>
+      <Image
+        style={{position: 'absolute', width, height: height * 0.4}}
+        source={{uri: article.urlToImage}}
+        resizeMode="cover"
+      />
+      {/* Shadow Box */}
+      <View
+        style={{
+          position: 'absolute',
+          top: 250,
+          alignItems: 'center',
+          width,
+          zIndex: 10,
+        }}>
+        <View
+          style={{
+            width: 311,
+            height: 141,
+            backgroundColor: 'rgba(245, 245, 245,0.9)',
+            borderRadius: 16,
+          }}></View>
+      </View>
+      {/* body */}
+      <View
+        style={{
+          width,
+          backgroundColor: colors.LIGHT,
+          height,
+          position: 'absolute',
+          top: 300,
+          borderTopRightRadius: 24,
+          borderTopLeftRadius: 24,
+        }}></View>
     </SafeAreaView>
   );
 };
