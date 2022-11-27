94d6e8313779d1b73b7af7850fa241e60a0cf1e5
mishenThakshana <mishen.git@gmail.com>
2022-11-27 08:57:38 +0530

    Logout button added in the profile screen

--- src/screens/main/Profile.tsx
+++ src/screens/main/Profile.tsx
@@ -1,11 +1,48 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useContext, useState} from 'react';
+import {View, SafeAreaView, useWindowDimensions} from 'react-native';
+import AwesomeAlert from 'react-native-awesome-alerts';
+import {Btn} from 'src/components';
+import {colors} from 'src/constants';
+import {UserContext} from 'src/context/UserContext';
+import styles from 'src/styles/Common.styles';
 
 const Profile = () => {
+  const {width} = useWindowDimensions();
+  const {setUser, setAuthenticated} = useContext<any>(UserContext);
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+
+  const logoutUser = () => {
+    AsyncStorage.removeItem('app_user');
+    setUser({});
+    setAuthenticated(false);
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView
+      style={[
+        styles.rootContainer,
+        {alignItems: 'center', justifyContent: 'center'},
+      ]}>
+      <View style={{width: width * 0.8}}>
+        <Btn handler={() => setShowAlert(true)} label="Logout" />
+      </View>
+      {/* Alert */}
+      <AwesomeAlert
+        show={showAlert}
+        showProgress={false}
+        title="Are you sure to logout?"
+        showCancelButton={true}
+        showConfirmButton={true}
+        closeOnTouchOutside={false}
+        closeOnHardwareBackPress={false}
+        cancelText="Cancel"
+        cancelButtonColor="#A9A9A9"
+        confirmButtonColor={colors.PRIMARY}
+        onConfirmPressed={logoutUser}
+        onCancelPressed={() => setShowAlert(false)}
+      />
+    </SafeAreaView>
   );
 };
 
